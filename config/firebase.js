9bc6ea1ea1bb652e4a18be8161159696e6ae0676
Travis Gorski <travisgorski@Traviss-MacBook-Pro.local>
2025-05-28 15:52:22 -0700

    stable with firebase workaround for Expo SDK 53

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,12 @@
 // ✅ Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeApp, getApps } from "firebase/app";
 import {
   initializeAuth,
   getAuth,
   getReactNativePersistence,
 } from "firebase/auth";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import {
   getFirestore,
   doc,
@@ -37,13 +38,14 @@ const firebaseConfig = {
 // ✅ Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-initializeAuth(app, {
+const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
 
+
 const storage = getStorage(app);
 const db = getFirestore(app);
-const auth = getAuth(app);
+
 
 // ✅ Create User Profile with EXP & Level
 export const createUserProfile = async (userId, email) => {
